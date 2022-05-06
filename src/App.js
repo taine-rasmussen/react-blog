4f4c6859f18fb317ac8a247609307427a33d8848
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-06 21:31:49 +1200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+// Components
+import Landing from './Components/Landing'
+
 function App() {
   return (
     <div className="App">
-
+      <Landing />
     </div>
   );
 }
---
+++ src/Components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Landing
