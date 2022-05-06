2f075fea96742599c441de8cfcea9f38c8994d18
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-07 00:14:24 +1200

    added syntax highlither

--- src/Components/StateBlog.js
+++ src/Components/StateBlog.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import SyntaxHighLighter from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
 import Textbox from '../Library/Textbox'
 import Button from '../Library/Button'
 
 const StateBlog = () => {
 
   const exampleText = 'this is some example text about useState'
+
+  const markdown = `A paragraph with *emphasis* and **strong importance**.`
 
   return (
     <div className="stateblog-container">
@@ -14,7 +19,12 @@ const StateBlog = () => {
         <Button text={'Home'} path={'/'} />
       </div>
       <div className="stateblog-body">
-        <Textbox text={exampleText} />
+        {/* <Textbox text={exampleText} /> */}
+        <SyntaxHighLighter language="javascript" style={dracula}>
+          {
+            "const func = () => {} {\n render() {\n return ( \n <div>\nHello {this.props.name}\n</div>\n);\n }\n}\n\nReactDOM.render(\n<HelloMessage name='Taylor' />,\ndocument.getElementById('hello-example')\n);"
+          }
+        </SyntaxHighLighter>
       </div>
     </div>
   )
