2a466dbf027c475db72bc80dfab5e7e765a6201f
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-07 13:31:13 +1200

    fixed body margin

--- src/Library/Button.js
+++ src/Library/Button.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 const Button = (props) => {
+  const {
+    text,
+    path
+  } = props
 
-   const {
-      text,
-      path
-   } = props
-
-   return (
-      <>
-         <Link to={path}><button>{text}</button></Link>
-      </>
-   )
+  return (
+    <>
+      <Link to={path}><button>{text}</button></Link>
+    </>
+  )
 }
 
 export default Button
